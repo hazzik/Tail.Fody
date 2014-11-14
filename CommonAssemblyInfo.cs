bb7e10e378ff0e14a1a36db7b93797f778e30246
Alexander Zaytsev <hazzik@gmail.com>
2014-11-15 00:54:11 +1300

    Set Version 0.0.2

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Tail")]
 [assembly: AssemblyProduct("Tail")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
